698ee2ec42175b21bf3a72c3866f9756fd067a00
godrose <godrose@gmail.com>
2016-03-20 17:25:45 +0200

    Added solution.

---
+++ src/LogoFX.Practices.IoC.Tests/TestParameterDependency.cs
@@ -0,0 +1,32 @@
+﻿namespace LogoFX.Practices.IoC.Tests
+{
+    interface ITestNamedParameterDependency
+    {
+        string Model { get; }
+    }
+
+    class TestNamedParameterDependency : ITestNamedParameterDependency
+    {
+        public TestNamedParameterDependency(string model)
+        {
+            Model = model;
+        }
+
+        public string Model { get; private set; }
+    }
+
+    interface ITestTypedParameterDependency
+    {
+        int Value { get; }
+    }
+
+    class TestTypedParameterDependency : ITestTypedParameterDependency
+    {
+        public TestTypedParameterDependency(int val)
+        {
+            Value = val;
+        }
+
+        public int Value { get; private set; }
+    }
+}
