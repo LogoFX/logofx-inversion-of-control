c41f3e09e2d49dc3aca4bafcf0c565b693ebd186
Gennady Verdel <godrose@gmail.com>
2017-12-03 09:06:24 +0200

    Added .NETStandard 2.0 support #4

--- src/LogoFX.Practices.IoC.NETStandard20.Tests/Properties/AssemblyInfo.cs
+++ src/LogoFX.Practices.IoC.NETStandard20.Tests/Properties/AssemblyInfo.cs
@@ -1,35 +1,35 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Practices.IoC")]
+[assembly: AssemblyTitle("LogoFX.Practices.IoC.NETStandard20.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LogoUI 2012")]
-[assembly: AssemblyProduct("LogoFX.Practices.IoC")]
-[assembly: AssemblyCopyright("Copyright LogoUI 2012©  2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogoFX.Practices.IoC.NETStandard20.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c484bd2b-1547-4657-91f3-568ab40256d7")]
+[assembly: Guid("dfd67cd9-6e12-427c-bcca-dee5a3522dc2")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.11")]
-[assembly: AssemblyFileVersion("1.0.11")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
