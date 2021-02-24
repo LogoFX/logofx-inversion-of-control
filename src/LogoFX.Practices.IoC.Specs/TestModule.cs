82ec5095161348d7872b155cac541ca02f5b135f
Gennady Verdel <godrose@gmail.com>
2021-02-24 22:06:55 +0200

    chore: renamed Tests to Specs #12

---
+++ src/LogoFX.Practices.IoC.Specs/TestModule.cs
@@ -0,0 +1,17 @@
+﻿namespace LogoFX.Practices.IoC.Specs
+{
+    interface ITestModule
+    {
+        string Name { get; set; }
+    }
+
+    class TestModule : ITestModule
+    {
+        public string Name { get; set; }
+    }
+
+    static class LifetimeProvider
+    {
+        internal static string Current;
+    }
+}
