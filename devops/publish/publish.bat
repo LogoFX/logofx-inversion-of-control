0d203e0984f7ad88fc0a3862f9f5a5d5b738e779
Gennady Verdel <godrose@gmail.com>
2021-04-02 12:42:54 +0300

    devops: added means for package creation and storage

--- devops/publish/publish.bat
+++ devops/publish/publish.bat
@@ -1,3 +1,5 @@
+SET package_name=LogoFX.Practices.IoC
+SET package_version=2.2.0-rc2
 cd ../build
 call build.bat
 cd ../test
@@ -5,6 +7,6 @@ call test-all.bat
 cd ../pack
 call ./pack.bat
 cd ../publish
-call ./copy.bat LogoFX.Practices.IoC 2.2.0-rc2 %1
+call ./copy.bat %package_name% %package_version% %1
 cd ../install
-call ./uninstall-global-single.bat LogoFX.Practices.IoC 2.2.0-rc2
+call ./uninstall-global-single.bat %package_name% %package_version%
