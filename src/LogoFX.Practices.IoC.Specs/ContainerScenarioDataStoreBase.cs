7a955bb1cda99acaef8932acf49fd3485e63cf8e
Gennady Verdel <godrose@gmail.com>
2021-06-21 20:11:17 +0300

    chore: bump deps #12

---
+++ src/LogoFX.Practices.IoC.Specs/ContainerScenarioDataStoreBase.cs
@@ -0,0 +1,42 @@
+﻿using Attest.Testing.Context.SpecFlow;
+using TechTalk.SpecFlow;
+
+namespace LogoFX.Practices.IoC.Specs
+{
+    internal sealed class ContainerScenarioDataStoreBase : ScenarioDataStoreBase
+    {
+        public ContainerScenarioDataStoreBase(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+        }
+
+        public ExtendedSimpleContainer Container
+        {
+            get => GetValue<ExtendedSimpleContainer>();
+            set => SetValue(value);
+        }
+
+        public object Dependency
+        {
+            get => GetValue<object>();
+            set => SetValue(value);
+        }
+
+        public object[] Dependencies
+        {
+            get => GetValue<object[]>();
+            set => SetValue(value);
+        }
+
+        public ITestModule FirstModule
+        {
+            get => GetValue<ITestModule>();
+            set => SetValue(value);
+        }
+
+        public ITestModule SecondModule
+        {
+            get => GetValue<ITestModule>();
+            set => SetValue(value);
+        }
+    }
+}
