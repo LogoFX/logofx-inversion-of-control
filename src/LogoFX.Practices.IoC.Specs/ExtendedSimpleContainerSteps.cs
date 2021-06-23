298ddb6635bf553309c6b285578dd700a112e2af
Gennady Verdel <godrose@gmail.com>
2021-06-23 06:38:59 +0300

    chore: bump deps

--- src/LogoFX.Practices.IoC.Specs/ExtendedSimpleContainerSteps.cs
+++ src/LogoFX.Practices.IoC.Specs/ExtendedSimpleContainerSteps.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using FluentAssertions;
 using TechTalk.SpecFlow;
@@ -8,24 +8,24 @@ namespace LogoFX.Practices.IoC.Specs
     [Binding]
     internal sealed class ExtendedSimpleContainerSteps
     {
-        private readonly ScenarioContext _scenarioContext;
+        private readonly ContainerScenarioDataStoreBase _scenarioDataStoreBase;
 
         public ExtendedSimpleContainerSteps(ScenarioContext scenarioContext)
         {
-            _scenarioContext = scenarioContext;
+            _scenarioDataStoreBase = new ContainerScenarioDataStoreBase(scenarioContext);
         }
 
         [When(@"The container is created")]
         public void WhenTheContainerIsCreated()
         {
             var container = new ExtendedSimpleContainer();
-            _scenarioContext.Add("container", container);
+            _scenarioDataStoreBase.Container = container;
         }
 
         [When(@"The dependency with named parameter is registered in transient fashion")]
         public void WhenTheDependencyWithNamedParameterIsRegisteredInTransientFashion()
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             container.RegisterPerRequest(typeof(ITestNamedParameterDependency), null,
                 typeof(TestNamedParameterDependency));
         }
@@ -33,7 +33,7 @@ namespace LogoFX.Practices.IoC.Specs
         [When(@"The dependency with typed parameter is registered in transient fashion")]
         public void WhenTheDependencyWithTypedParameterIsRegisteredInTransientFashion()
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             container.RegisterPerRequest(typeof(ITestTypedParameterDependency), null,
                 typeof(TestTypedParameterDependency));
         }
@@ -41,61 +41,61 @@ namespace LogoFX.Practices.IoC.Specs
         [When(@"The dependency with named parameter is registered in singleton fashion")]
         public void WhenTheDependencyWithNamedParameterIsRegisteredInSingletonFashion()
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             container.RegisterSingleton<ITestModule>((c, r) => new TestModule {Name = "1"});
         }
 
         [When(@"The same type dependency is registered in instance fashion using first instance")]
         public void WhenTheSameTypeDependencyIsRegisteredInInstanceFashionUsingFirstInstance()
         {
-            RegisterInstanceDependency("firstModule", new TestModule {Name = "1"});
+            RegisterInstanceDependency((r, v) => r.FirstModule = v, new TestModule {Name = "1"});
         }
 
         [When(@"The same type dependency is registered in instance fashion using second instance")]
         public void WhenTheSameTypeDependencyIsRegisteredInInstanceFashionUsingSecondInstance()
         {
-            RegisterInstanceDependency("secondModule", new TestModule { Name = "2" });
+            RegisterInstanceDependency((r, v) => r.SecondModule = v, new TestModule { Name = "2" });
         }
 
-        private void RegisterInstanceDependency(string key, ITestModule instance)
+        private void RegisterInstanceDependency(Action<ContainerScenarioDataStoreBase, ITestModule> valueSetter, ITestModule instance)
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             var module = new TestModule { Name = "1" };
             container.RegisterHandler(typeof(ITestModule), null, (c, r) => instance);
-            _scenarioContext.Add(key, instance);
+            valueSetter(_scenarioDataStoreBase, instance);
         }
 
         [When(@"The dependency is resolved with value '(.*)' for named parameter")]
         public void WhenTheDependencyIsResolvedWithValueForNamedParameter(string parameter)
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             var dependency = container.GetInstance(typeof(ITestNamedParameterDependency), null,
                 new IParameter[] { new NamedParameter("model", parameter) }) as ITestNamedParameterDependency;
-            _scenarioContext.Add("dependency", dependency);
+            _scenarioDataStoreBase.Dependency = dependency;
         }
 
         [When(@"The dependency is resolved with value (.*) for typed parameter")]
         public void WhenTheDependencyIsResolvedWithValueForTypedParameter(int parameter)
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             const int val = 6;
             var dependency = container.GetInstance(typeof(ITestTypedParameterDependency), null,
                 new IParameter[] { new TypedParameter(typeof(int), val) }) as ITestTypedParameterDependency;
-            _scenarioContext.Add("dependency", dependency);
+            _scenarioDataStoreBase.Dependency = dependency;
         }
 
         [When(@"The collection of dependencies is resolved")]
         public void WhenTheCollectionOfDependenciesIsResolved()
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             var dependencies = container.GetAllInstances(typeof(ITestModule)).ToArray();
-            _scenarioContext.Add("dependencies", dependencies);
+            _scenarioDataStoreBase.Dependencies = dependencies;
         }
 
         [Then(@"Actual value of parameter inside the named dependency is '(.*)'")]
         public void ThenActualValueOfParameterInsideTheNamedDependencyIs(string parameter)
         {
-            var dependency = _scenarioContext.Get<ITestNamedParameterDependency>("dependency");
+            var dependency = _scenarioDataStoreBase.Dependency as ITestNamedParameterDependency;
             var actualModel = dependency.Model;
             actualModel.Should().Be(parameter);
         }
@@ -103,7 +103,7 @@ namespace LogoFX.Practices.IoC.Specs
         [Then(@"Actual value of parameter inside the typed dependency is (.*)")]
         public void ThenActualValueOfParameterInsideTheTypedDependencyIs(int parameter)
         {
-            var dependency = _scenarioContext.Get<ITestTypedParameterDependency>("dependency");
+            var dependency = _scenarioDataStoreBase.Dependency as ITestTypedParameterDependency;
             var actualValue = dependency.Value;
             actualValue.Should().Be(parameter);
         }
@@ -111,9 +111,9 @@ namespace LogoFX.Practices.IoC.Specs
         [Then(@"The collection of dependencies is equivalent to the collection of instances")]
         public void ThenTheCollectionOfDependenciesIsEquivalentToTheCollectionOfInstances()
         {
-            var dependencies = _scenarioContext.Get<IEnumerable<object>>("dependencies").ToArray();
+            var dependencies = _scenarioDataStoreBase.Dependencies;
             var modules = new[]
-                {_scenarioContext.Get<ITestModule>("firstModule"), _scenarioContext.Get<ITestModule>("secondModule")};
+                {_scenarioDataStoreBase.FirstModule, _scenarioDataStoreBase.SecondModule};
             dependencies.Should().BeSubsetOf(modules);
             modules.Should().BeSubsetOf(dependencies);
         }
@@ -121,7 +121,7 @@ namespace LogoFX.Practices.IoC.Specs
         [Then(@"Multiple dependency resolutions yield same value")]
         public void ThenMultipleDependencyResolutionsYieldSameValue()
         {
-            var container = _scenarioContext.Get<ExtendedSimpleContainer>("container");
+            var container = _scenarioDataStoreBase.Container;
             var module = container.GetInstance(typeof(ITestModule), null);
             var actualModule = container.GetInstance(typeof(ITestModule), null);
 
